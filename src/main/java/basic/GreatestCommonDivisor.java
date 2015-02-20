7bbfee2224c086eb965488e1a9f6fd1b37a96432
golovko_yakov <golovko.yakov@gmail.com>
2015-02-21 01:35:23 +0200

    added the algorithm for search the greatest common divisor

---
+++ src/main/java/basic/GreatestCommonDivisor.java
@@ -0,0 +1,22 @@
+package main.java.basic;
+
+public class GreatestCommonDivisor {
+    private static int gcdRecursive(int p, int q){
+        if (q == 0) {return p;}
+        return gcdRecursive(q, p%q);
+    }
+
+    private static int gcdNonRecursive(int p, int q){
+        while (q!=0){
+            int tmp = p%q;
+            p = q;
+            q = tmp;
+        }
+        return p;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(gcdRecursive(28,12));
+        System.out.println(gcdNonRecursive(28, 12));
+    }
+}
