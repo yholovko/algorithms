ef6d431738a266007f4a5938e8724fdc69190bf6
golovko_yakov <golovko.yakov@gmail.com>
2015-03-02 22:54:16 +0200

    added Dijkstra's Two - Stack Algorithm for Expression Evaluation

---
+++ src/main/java/basic/DijkstraExpressionEvaluation.java
@@ -0,0 +1,49 @@
+package main.java.basic;
+
+import java.util.Stack;
+
+/**
+ * Dijkstra's Two - Stack Algorithm for Expression Evaluation
+ * * 
+ */
+public class DijkstraExpressionEvaluation {
+    private static Stack<String> ops = new Stack<>();
+    private static Stack<Double> val = new Stack<>();
+    
+    public static void main(String[] args) {
+        String expression = "( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) )";
+        
+        for (String s : expression.split(" ")){
+            if (s.equals("(")) continue;
+            switch (s) {
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                    ops.push(s);
+                    break;
+                case ")":
+                    switch (ops.pop()) {
+                        case "+":
+                            val.push(val.pop() + val.pop());
+                            break;
+                        case "-":
+                            val.push(val.pop() - val.pop());
+                            break;
+                        case "*":
+                            val.push(val.pop() * val.pop());
+                            break;
+                        case "/":
+                            val.push(val.pop() / val.pop());
+                            break;
+                    }
+                    break;
+                default:
+                    val.push(Double.valueOf(s));
+                    break;
+            }
+        }
+
+        System.out.println("Result = " + val.pop());
+    }
+}
